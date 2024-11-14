4bab43b5dbcce851b14871ee413fb1b2cb7056b0
YuSeungYeol <sr.0220a@gmail.com>
2024-11-14 14:12:11 +0900

    [유승열] service 수정

--- individual_project/src/main/java/com/sy/spring/smokingArea/service/SmokingAreaService.java
+++ individual_project/src/main/java/com/sy/spring/smokingArea/service/SmokingAreaService.java
@@ -16,7 +16,6 @@ public class SmokingAreaService {
     public SmokingAreaService(SmokingAreaRepository smokingAreaRepository) {
         this.smokingAreaRepository = smokingAreaRepository;
     } 
-
     // 기본 위치 (서울) 기준으로 근처 흡연장 가져오기
     public List<SmokingArea> getSmokingAreas(double latitude, double longitude) {
         // 실제 위치 기반으로 가까운 흡연장 데이터 가져오기
