50772a8761be9035fffcbc47f1241b343532c05e
YuSeungYeol <sr.0220a@gmail.com>
2024-11-27 12:16:49 +0900

    [유승열] repository 수정

--- individual_project/src/main/java/com/sy/spring/smokingArea/repository/SmokingAreaRepository.java
+++ individual_project/src/main/java/com/sy/spring/smokingArea/repository/SmokingAreaRepository.java
@@ -9,7 +9,7 @@ public interface SmokingAreaRepository extends JpaRepository<SmokingArea, Long> 
 
     // 주소를 포함하는 흡연장 검색
     List<SmokingArea> findByAddressContaining(String address);
- 
+
     // 위치 기반 흡연장 검색 (거리 계산 로직은 별도로 추가 필요)
     List<SmokingArea> findNearbySmokingAreas(double latitude, double longitude);
 }
