eb5a6e132d16c4be33ffacf46b361856a6cf83c8
YuSeungYeol <sr.0220a@gmail.com>
2024-11-27 13:51:14 +0900

    [유승열] repository 변경

--- individual_project/src/main/java/com/sy/spring/smokingArea/repository/SmokingAreaRepository.java
+++ individual_project/src/main/java/com/sy/spring/smokingArea/repository/SmokingAreaRepository.java
@@ -2,6 +2,7 @@ package com.sy.spring.smokingArea.repository;
 
 import com.sy.spring.smokingArea.domain.SmokingArea;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,6 +11,8 @@ public interface SmokingAreaRepository extends JpaRepository<SmokingArea, Long> 
     // 주소를 포함하는 흡연장 검색
     List<SmokingArea> findByAddressContaining(String address);
 
-    // 위치 기반 흡연장 검색 (거리 계산 로직은 별도로 추가 필요)
-    List<SmokingArea> findNearbySmokingAreas(double latitude, double longitude);
+    // 위치 기반 흡연장 검색 (거리 계산 로직 포함)
+    @Query("SELECT s FROM SmokingArea s WHERE " +
+           "POW((s.latitude - :latitude), 2) + POW((s.longitude - :longitude), 2) < POW(:radius, 2)")
+    List<SmokingArea> findNearbySmokingAreas(double latitude, double longitude, double radius);
 }
