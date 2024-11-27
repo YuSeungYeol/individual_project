9add9520b1f71c200e5e94d32cd82c17ef9062d1
YuSeungYeol <sr.0220a@gmail.com>
2024-11-27 13:50:56 +0900

    [유승열] service 변경

--- individual_project/src/main/java/com/sy/spring/smokingArea/service/SmokingAreaService.java
+++ individual_project/src/main/java/com/sy/spring/smokingArea/service/SmokingAreaService.java
@@ -15,17 +15,16 @@ public class SmokingAreaService {
     @Autowired
     public SmokingAreaService(SmokingAreaRepository smokingAreaRepository) {
         this.smokingAreaRepository = smokingAreaRepository;
-    } 
+    }
 
     // 기본 위치 (서울) 기준으로 근처 흡연장 가져오기
     public List<SmokingArea> getSmokingAreas(double latitude, double longitude) {
-        // 실제 위치 기반으로 가까운 흡연장 데이터 가져오기
-        return smokingAreaRepository.findNearbySmokingAreas(latitude, longitude);
+        double radius = 1000; // 예를 들어 1000m 내에서 검색
+        return smokingAreaRepository.findNearbySmokingAreas(latitude, longitude, radius);
     }
- 
+
     // 검색된 주소를 기준으로 흡연장 검색
     public List<SmokingArea> searchSmokingAreas(String searchQuery) {
-        // 장소명으로 흡연장 검색
         return smokingAreaRepository.findByAddressContaining(searchQuery);
     }
 }
